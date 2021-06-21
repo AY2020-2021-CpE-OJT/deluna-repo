f58ce5e72cdab1ba94bf3649d05d937b3650ff01
rjmdeluna <rjmdeluna@gmail.com>
2021-06-22 03:17:38 +0800

    added new line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello Philippines, I am Rojen! I love the galaxy");
+		System.out.println("The world collides.")
 	}
 }
